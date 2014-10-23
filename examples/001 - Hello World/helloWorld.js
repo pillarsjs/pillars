cfa134b64b5a2fbe544f8ed95973c8a24dfbbb5b
lilxelo <cheloq@gmail.com>
2014-10-23 20:49:17 +0200

    Documentation

--- examples/001 - Hello World/helloWorld.js
+++ examples/001 - Hello World/helloWorld.js
@@ -2,11 +2,13 @@ var pillars = require('../../pillars.js').global().configure({
 	templatesCache:false
 });
 
-var app = new App().start()
-	.add(new Pillar()
-		.add(new Beam(function(gw){
-			console.log(PILLARS.path);
-			gw.send("holaa");
-		}))
-	)
+var app = new App().start();
+var pillar = new Pillar();
+
+app.add(pillar
+	.add(new Beam(function(gw){
+		console.log(pillar.path, pillar.id, pillar.priority, pillar.host, pillar.beams);
+		gw.send("holaa");
+	}))
+)
 ;
