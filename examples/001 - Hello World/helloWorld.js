1df30044437f8e1dc5e8c28c88c020cf294521c8
lilxelo <cheloq@gmail.com>
2014-12-26 20:00:55 +0100

    Documentation

--- examples/001 - Hello World/helloWorld.js
+++ examples/001 - Hello World/helloWorld.js
@@ -1,3 +1,12 @@
 require('../../pillars.js').start();
 
 ENV.languages = ['es','en'];
+ENV.templates.cache = false;
+
+
+route = new Route(function(gw){gw.send("Hola Mundo")});
+console.log(route.id);
+//Imprime: i45w7qt1
+route.id = "myRoute";
+console.log(route.id);
+//Imprime: myRoute
