83e5c050a7a2f4270cb79b7b4e5dc965b34e1533
Bifuer <bifuer@odeux.com>
2015-02-22 13:27:38 +0100

    first stage of v0.1 refactor. (cleaning...)

--- index.js
+++ index.js
@@ -109,7 +109,41 @@ logger.info('plugins.loaded',{list:plugins});
 global.modelator = require('./lib/modelator');
 
 // Mailer
-global.mailer = require('./lib/Mailer');
+var nodemailer = require('nodemailer');
+
+var mailTransport;
+Object.defineProperty(ENV,"smtp",{
+	enumerable : true,
+	get : function(){return mailTransport;},
+	set : function(set){
+		mailTransport = nodemailer.createTransport(set);
+	}
+});
+
+var mailer = global.mailer = {};
+
+mailer.send = function(mail,callback){
+	if(!mail.from && ENV.administrator && ENV.administrator.mail){
+		mail.from = ENV.administrator.mail;
+		if(ENV.administrator.firstname){
+			var fromName = (ENV.administrator.lastname)?ENV.administrator.firstname+' '+ENV.administrator.lastname:ENV.administrator.firstname;
+			mail.from = fromName+' <'+mail.from+'>';
+		}
+	}
+	if(mailTransport){
+		ENV.emit('mail',mail);
+		mailTransport.sendMail(mail, function(error,info){
+			if(callback){callback(error,info);}
+		});
+	} else {
+		if(callback){callback(new Error(i18n('pillars.mail.no-transport')));}
+	}
+};
+
+
+
+
+
 
 // Cron
 //global.cron = require('./lib/Cron');
