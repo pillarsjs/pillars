96b4d5bea4f89b628a5b205ac406517e933fdfb0
Javier GM <bifuer@gmail.com>
2016-05-20 12:00:39 +0200

    cors auto allow headers response

--- plugins/CORS.js
+++ plugins/CORS.js
@@ -13,8 +13,7 @@ var plugin = module.exports = new Plugin({
     if (cors === true || (Array.isArray(cors) && cors.indexOf(gw.origin) >= 0)) {
       gw.cors.origin = gw.origin;
       gw.cors.credentials = true;
-
-      gw.cors.headers = gw.req.headers['Access-Control-Request-Headers'] || false;
+      gw.cors.headers = gw.req.headers['access-control-request-headers']?gw.req.headers['access-control-request-headers'].split(','):false;
       gw.cors.methods = Array.isArray(gw.routing.inheritance.method)?gw.routing.inheritance.method.concat(['OPTIONS', 'HEAD']):['GET','PUT','POST','DELETE','OPTIONS', 'HEAD'];
     }
   }
